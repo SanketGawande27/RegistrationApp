19c83c9a57f6fc20691945ced17a8d102a7b0b24
SanketGawande27 <sam2219kg@gmail.com>
2023-08-13 17:26:44 +0530

    Created new Registration App

---
+++ app/src/main/java/com/example/registrationapp/Login.java
@@ -0,0 +1,24 @@
+package com.example.registrationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        TextView btn = findViewById(R.id.txtSignUp);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this, Registration.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/registrationapp/Registration.java
@@ -0,0 +1,24 @@
+package com.example.registrationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+        TextView btn = findViewById(R.id.txtAlreadyHaveAccount);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Registration.this, Login.class));
+            }
+        });
+    }
+}
