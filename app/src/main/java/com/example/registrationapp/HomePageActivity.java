dfe1e70471698168c0078cdda623b1728e350029
SanketGawande27 <sam2219kg@gmail.com>
2023-08-19 19:11:35 +0530

    all working code with location permission

--- app/src/main/java/com/example/registrationapp/HomePageActivity.java
+++ app/src/main/java/com/example/registrationapp/HomePageActivity.java
@@ -5,12 +5,17 @@ import androidx.core.app.ActivityCompat;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -24,13 +29,24 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Timer;
 import java.util.TimerTask;
+
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 public class HomePageActivity extends AppCompatActivity {
 
-    TextView showtimer, locationView;
+    TextView showtimer, locationView, diplayInOutlabel;
     Double time = 0.0;
     TimerTask timerTask;
     String inTime = null;
@@ -44,21 +60,33 @@ public class HomePageActivity extends AppCompatActivity {
     Button markeAttendanceLogin;
     Button markeAttendanceLogout;
     Boolean alreadyMarkedLogout=true;
-    // initializing FusedLocationProviderClient object
+
    FusedLocationProviderClient fusedLocationClient;
     private final static int REQUEST_CODE = 100;
+    private LocationRequest locationRequest;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-       // ActivityCompat.requestPermissions( this, new String[] {android.Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        //for location request
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(5000);
+        locationRequest.setFastestInterval(2000);
+        //end location request
+
+        /* ----------- initialized variables ------------------- */
         timer = new Timer();
         markeAttendanceLogin = (Button) findViewById(R.id.btnAttendanceMarked);
         markeAttendanceLogout = (Button) findViewById(R.id.btnMakredAttendanceLogout);
+        markeAttendanceLogout.setEnabled(false);
         locationView = (TextView) findViewById(R.id.locationView);
+        diplayInOutlabel = (TextView) findViewById(R.id.diplayInOutlabel);
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         showtimer = (TextView) findViewById(R.id.textTimer);
+        /* ----------- end initialized variables ------------------- */
         markeAttendanceLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -66,13 +94,15 @@ public class HomePageActivity extends AppCompatActivity {
                 String currentTime = new SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(new Date());
                 inTime = currentDate+ " - "+currentTime;
                 markeAttendanceLogin.setEnabled(false);
+                markeAttendanceLogout.setEnabled(true);
                 if(!alreadyMarkedLogout){
                     markeAttendanceLogout.setEnabled(true);
                     time = 0.0;
                     showtimer.setText(formatTime(0,0,0));
                 }
                 // method to get the location
-                getLastLocation();
+                diplayInOutlabel.setText("Your Current in-Time location details");
+                getCurentLocation();
                 startTimer();  // for showing timer in screen
                Toast.makeText(HomePageActivity.this, "Login Attendance Marked for today's date : " + inTime, Toast.LENGTH_SHORT).show();
             }
@@ -88,58 +118,97 @@ public class HomePageActivity extends AppCompatActivity {
                 markeAttendanceLogin.setEnabled(true);
                 alreadyMarkedLogout = false;
                 timerTask.cancel();
-                if(locationView.getText().toString() != null){
-                        locationView.setText("");
-                }
+                diplayInOutlabel.setText("Your Current out-Time locatioin details");
+                //getLastLocation();
+                getCurentLocation();
                 Toast.makeText(HomePageActivity.this, "Logout Attendance Marked for today's date : "  +outTime, Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-    private void getLastLocation() {
-        if(ContextCompat.checkSelfPermission(this,
-                android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+    private void getCurentLocation() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(HomePageActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                if (isGPSEnabled()) {
 
-            fusedLocationClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
-                @Override
-                public void onSuccess(Location location) {
-                    if(location != null){
-                        Geocoder geocoder = new Geocoder(HomePageActivity.this, Locale.getDefault());
-                        List<Address> addresses = null;
-                        try {
-                            addresses = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
-                            Log.e("Address : ","Latitude : " + addresses.get(0).getLatitude()+ "Longitude : " +addresses.get(0).getLongitude() );
-                            locationView.setText("Your Current in-Time Locatioin \n"+"Latitude : " + addresses.get(0).getLatitude() + "\nLongitude : " +addresses.get(0).getLongitude() + "\nAddress Location : "+ addresses.get(0).getAddressLine(0));
+                    LocationServices.getFusedLocationProviderClient(HomePageActivity.this).requestLocationUpdates(locationRequest, new LocationCallback() {
+                        @Override
+                        public void onLocationResult(@NonNull LocationResult locationResult) {
+                            super.onLocationResult(locationResult);
+                            LocationServices.getFusedLocationProviderClient(HomePageActivity.this).removeLocationUpdates(this);
 
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
+                            if(locationResult != null && locationResult.getLocations().size() >0){
+                                int index = locationResult.getLocations().size() -1;
+                                double latitude = locationResult.getLocations().get(index).getLatitude();
+                                double longitude = locationResult.getLocations().get(index).getLongitude();
+                                locationView.setText("Latitude : " + latitude+ "\n" + "Longitude : "+ longitude);
+
+                            }
                         }
-                    }
+                    }, Looper.getMainLooper());
+
+
+                } else {
+                    turnOnGPS();
                 }
-            });
-        }else {
-                askPermission();
+            } else {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 100);
+            }
         }
     }
 
-    private void askPermission() {
-        ActivityCompat.requestPermissions(HomePageActivity.this,new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+    private boolean isGPSEnabled() {
+
+        LocationManager locationManager1 = null;
+        boolean isEnabled = false;
+        if (locationManager1 == null) {
+            locationManager1 = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        }
+        isEnabled = locationManager1.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        return isEnabled;
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull  String[] permissions, @NonNull  int[] grantResults) {
-       if(requestCode == REQUEST_CODE){
+    private void turnOnGPS() {
 
-           if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
 
-               getLastLocation();
-           }else {
-               Toast.makeText(HomePageActivity.this, "Please provide the request permission", Toast.LENGTH_SHORT).show();
-           }
-       }
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+        builder.setAlwaysShow(true);
 
-       super.onRequestPermissionsResult(requestCode,permissions,grantResults);
+        Task<LocationSettingsResponse> result = LocationServices.getSettingsClient(getApplicationContext())
+                .checkLocationSettings(builder.build());
+
+        result.addOnCompleteListener(new OnCompleteListener<LocationSettingsResponse>() {
+            @Override
+            public void onComplete(@NonNull Task<LocationSettingsResponse> task) {
+
+                try {
+                    LocationSettingsResponse response = task.getResult(ApiException.class);
+                    Toast.makeText(HomePageActivity.this, "GPS is already tured on", Toast.LENGTH_SHORT).show();
+
+                } catch (ApiException e) {
+
+                    switch (e.getStatusCode()) {
+                        case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+
+                            try {
+                                ResolvableApiException resolvableApiException = (ResolvableApiException) e;
+                                resolvableApiException.startResolutionForResult(HomePageActivity.this, 2);
+                            } catch (IntentSender.SendIntentException ex) {
+                                ex.printStackTrace();
+                            }
+                            break;
+
+                        case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                            //Device does not have location
+                            break;
+                    }
+                }
+            }
+        });
     }
+
 
     private void startTimer() {
         timerTask = new TimerTask()
--- app/src/main/java/com/example/registrationapp/Login.java
+++ app/src/main/java/com/example/registrationapp/Login.java
@@ -1,9 +1,18 @@
 package com.example.registrationapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,29 +20,78 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.registrationapp.data.MyDBHandler;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 import org.w3c.dom.Text;
 
 public class Login extends AppCompatActivity {
 
     EditText username, password;
-
+    TextView btn;
     TextView forgetpass;
     Button login;
 
     MyDBHandler db;
-
+    private LocationRequest locationRequest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(5000);
+        locationRequest.setFastestInterval(2000);
 
+        setContentView(R.layout.activity_login);
         username = (EditText) findViewById(R.id.txtUsername);
         password = (EditText) findViewById(R.id.txtPassword);
         login = (Button) findViewById(R.id.btn_login);
+        btn = findViewById(R.id.txtSignUp);
         forgetpass = (TextView) findViewById(R.id.txtForgetPassword);
         db = new MyDBHandler(this);
 
+
+        // for enable GPS
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(Login.this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                if (isGPSEnabled()) {
+
+                           /* LocationServices.getFusedLocationProviderClient(Login.this).requestLocationUpdates(locationRequest, new LocationCallback() {
+                                @Override
+                                public void onLocationResult(@NonNull LocationResult locationResult) {
+                                    super.onLocationResult(locationResult);
+                                    LocationServices.getFusedLocationProviderClient(Login.this).removeLocationUpdates(this);
+
+                                    if(locationResult != null && locationResult.getLocations().size() >0){
+                                        int index = locationResult.getLocations().size() -1;
+                                        double latitude = locationResult.getLocations().get(index).getLatitude();
+                                        double longitude = locationResult.getLocations().get(index).getLongitude();
+                                        viewlocation.setText("Latitude : " + latitude+ "\n" + "Longitude : "+ longitude);
+
+                                    }
+                                }
+                            }, Looper.getMainLooper());  */
+
+
+                } else {
+                    turnOnGPS();
+                }
+            } else {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 100);
+            }
+        }
+
+        // end enable GPS code
 
         forgetpass.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,12 +122,62 @@ public class Login extends AppCompatActivity {
             }
         });
 
-        TextView btn = findViewById(R.id.txtSignUp);
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(Login.this, Registration.class));
             }
         });
+    }
+
+    private void turnOnGPS() {
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+        builder.setAlwaysShow(true);
+
+        Task<LocationSettingsResponse> result = LocationServices.getSettingsClient(getApplicationContext())
+                .checkLocationSettings(builder.build());
+
+        result.addOnCompleteListener(new OnCompleteListener<LocationSettingsResponse>() {
+            @Override
+            public void onComplete(@NonNull Task<LocationSettingsResponse> task) {
+
+                try {
+                    LocationSettingsResponse response = task.getResult(ApiException.class);
+                    Toast.makeText(Login.this, "GPS is already tured on", Toast.LENGTH_SHORT).show();
+
+                } catch (ApiException e) {
+
+                    switch (e.getStatusCode()) {
+                        case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+
+                            try {
+                                ResolvableApiException resolvableApiException = (ResolvableApiException) e;
+                                resolvableApiException.startResolutionForResult(Login.this, 2);
+                            } catch (IntentSender.SendIntentException ex) {
+                                ex.printStackTrace();
+                            }
+                            break;
+
+                        case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                            //Device does not have location
+                            break;
+                    }
+                }
+            }
+        });
+    }
+
+    private boolean isGPSEnabled() {
+
+        LocationManager locationManager1 = null;
+        boolean isEnabled = false;
+        if (locationManager1 == null) {
+            locationManager1 = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        }
+        isEnabled = locationManager1.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        return isEnabled;
     }
 }
